4635d81df4902ee1ced73a78f3b00fc72368bd56
Evan Wang <evanwang@princeton.edu>
2024-01-05 11:12:25 -0500

    negclip fix

--- vision-language-models-are-bows/model_zoo/__init__.py
+++ vision-language-models-are-bows/model_zoo/__init__.py
@@ -130,7 +130,7 @@ def get_model(model_name, device, root_dir=CACHE_DIR):
         image_preprocess = None
         return flava_model, image_preprocess
 
-    elif model_name == "NegCLIP":
+    elif "NegCLIP" in model_name:
         import open_clip
         from .clip_models import CLIPWrapper
 
