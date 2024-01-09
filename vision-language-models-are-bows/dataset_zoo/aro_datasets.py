028973cf5aa8e3ddbc84eb855b4be1024af32110
Evan Wang <evanwang@princeton.edu>
2024-01-09 16:34:44 -0500

    gpt0

--- vision-language-models-are-bows/dataset_zoo/aro_datasets.py
+++ vision-language-models-are-bows/dataset_zoo/aro_datasets.py
@@ -39,9 +39,14 @@ class VG_Relation(Dataset):
 
         # annotation_file = os.path.join(root_dir, "visual_genome_relation.json")
 
+        # annotation_file = os.path.join(
+        #     "/scratch/gpfs/evanwang/CompVLMs/vision-language-models-are-bows/my_captions",
+        #     "rel-modified-fixed-final-1.json",
+        # )
+
         annotation_file = os.path.join(
             "/scratch/gpfs/evanwang/CompVLMs/vision-language-models-are-bows/my_captions",
-            "rel-modified-fixed-final-1.json",
+            "rel-gpt-0.json",
         )
 
         # annotation_file = os.path.join(
